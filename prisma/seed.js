d2b88097886f0b1cfa9710a98f3f4fd6e1668840
myc37 <68676763+myc37@users.noreply.github.com>
2022-01-27 21:17:58 +0800

    Basic design for people/[id]

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,29 +1,41 @@
-import pkg from "@prisma/client";
-const { PrismaClient } = pkg;
-import fetch from "node-fetch";
+const { PrismaClient } = require("@prisma/client");
+const fetch = require("node-fetch");
 
 const prisma = new PrismaClient();
 
 async function main() {
 	const dataList = [];
 	for (let i = 0; i < 83; i++) {
-		await fetch(`https://swapi.dev/api/people/${i + 1}`)
-			.then((res) => res.json())
-			.then((resJson) => {
-				if (!resJson.detail) {
-					dataList.push({
-						id: i + 1,
-						name: resJson.name,
-						gender: resJson.gender,
-						hairColour: resJson.hair_color,
-						skinColour: resJson.skin_color,
-						height: resJson.height,
-						mass: resJson.mass,
-					});
-				}
+		const swapi = await fetch(`https://swapi.dev/api/people/${i + 1}`).then(
+			(res) => res.json()
+		);
+		if (!swapi.detail) {
+			const akabab = await fetch(
+				`https://akabab.github.io/starwars-api/api/id/${i + 1}.json`
+			).then((res) => res.json());
+
+			console.log(`${i + 1} ${swapi.name}`);
+
+			dataList.push({
+				id: i + 1,
+				name: swapi.name,
+				species: akabab.species,
+				gender: swapi.gender,
+				height:
+					swapi.height === "unknown"
+						? swapi.height
+						: swapi.height + "cm",
+				mass: swapi.mass === "unknown" ? swapi.mass : swapi.mass + "kg",
+				hairColour: swapi.hair_color,
+				skinColour: swapi.skin_color,
+				eyeColour: akabab.eyeColor || "unknown",
+				image: akabab.image,
+				homeWorld: Array.isArray(akabab.homeworld)
+					? akabab.homeworld.reduce((x, y) => x + " & " + y)
+					: akabab.homeworld || "unknown",
 			});
+		}
 	}
-	// console.log(dataList);
 	await prisma.people.createMany({
 		data: dataList,
 	});
--- src/pages/people/[id].js
+++ src/pages/people/[id].js
@@ -1,23 +1,67 @@
 import prisma from "../../../lib/prisma";
 import Head from "next/head";
+import Image from "next/image";
+import { useState } from "react";
 
 export default function Person({ data }) {
+	const [imgSrc, setImgSrc] = useState(data.image);
+
 	return (
 		<div>
 			<Head>
-				<title>{data.name} Profile</title>
+				<link rel="icon" href="/light-saber.png" />
+				<title>{data.name}</title>
 			</Head>
-			<div className="text-center">
-				<div className="bg-red-200">name: {data.name}</div>
-				<div className="bg-red-200">gender: {data.gender}</div>
-				<div className="bg-green-200">
-					hair colour: {data.hairColour}
+			<div className="flex flex-col capitalize text-center text-white">
+				<div className="text-3xl p-10">{data.name}</div>
+				<div className="flex flex-col gap-y-12 mb-8 justify-center items-center md:gap-x-24 xl:gap-x-36 lg:flex-row">
+					<div className="w-5/6 sm:w-2/3 lg:w-1/3 2xl:w-1/4">
+						<Image
+							src={imgSrc}
+							alt={data.name}
+							height={750}
+							width={500}
+							layout="responsive"
+							onError={() => {
+								setImgSrc("/fallback.png");
+							}}
+						/>
+					</div>
+					<div className="grid gap-8 sm:grid-cols-3 sm:grid-flow-row sm:gap-12 md:gap-24 text-2xl">
+						<div>
+							<p className="category-title">Species</p>
+							<p>{data.species}</p>
+						</div>
+						<div>
+							<p className="category-title">Home World</p>
+							<p>{data.homeWorld}</p>
+						</div>
+						<div>
+							<p className="category-title">Gender</p>
+							<p>{data.gender}</p>
+						</div>
+						<div>
+							<p className="category-title">Skin Colour</p>
+							<p>{data.skinColour}</p>
+						</div>
+						<div>
+							<p className="category-title">Hair Colour</p>
+							<p>{data.hairColour}</p>
+						</div>
+						<div>
+							<p className="category-title">Eye Colour</p>
+							<p>{data.eyeColour}</p>
+						</div>
+						<div>
+							<p className="category-title">Height</p>
+							<p>{data.height}</p>
+						</div>
+						<div>
+							<p className="category-title">Mass</p>
+							<p>{data.mass}</p>
+						</div>
+					</div>
 				</div>
-				<div className="bg-green-200">
-					skin colour: {data.skinColour}
-				</div>
-				<div className="bg-blue-200">height: {data.height}</div>
-				<div className="bg-blue-200">mass: {data.mass}</div>
 			</div>
 		</div>
 	);
