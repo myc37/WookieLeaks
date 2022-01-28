412287921ebecf3142541881bab4ccb4a0758359
myc37 <68676763+myc37@users.noreply.github.com>
2022-01-28 18:11:59 +0800

    Add Shadow Database URL

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,11 +1,9 @@
 export default function Background() {
 	return (
 		<div className="star-field col-center">
-			<div className="layer"></div>
-			<div className="layer"></div>
-			<div className="layer"></div>
-			<div className="layer"></div>
-			<div className="layer"></div>
+			{[...Array(5)].map((_, index) => {
+				return <div key={index} className="layer"></div>;
+			})}
 		</div>
 	);
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,23 @@
 import { useRouter } from "next/router";
 import Page from "../components/Page";
+import Image from "next/image";
 
 export default function Home() {
 	const router = useRouter();
 	return (
 		<Page title="WookieLeaks">
-			<button
-				className="text-yellow-400 z-10"
-				onClick={() => router.push("/people")}
-			>
-				People
-			</button>
+			<div>Discover the</div>
+			<div>People of</div>
+			<div className="w-1/6">
+				<Image
+					src="/swlogo.png"
+					alt="swlogo"
+					height={1}
+					width={2}
+					layout="responsive"
+					className="bg-black"
+				/>
+			</div>
 		</Page>
 	);
 }
